793e6a0a99b696ee09c23d7df4ab9099fddba050
mrfreak94 <bitrix9494@gmail.com>
2020-05-27 13:28:04 +0200

    data-preparation-correction

--- Data_preparation.py
+++ Data_preparation.py
@@ -17,17 +17,23 @@ cases_removed=data[data["project_id"]=="TCGA-SARC"]
 data= data[data["project_id"]!="TCGA-SARC"]
 data["is_tumor"]=data["is_tumor"].map({0: 'sane', 1: 'tumor'})
 data["label"]=data.apply(lambda row: union(row),axis=1)
+data=data.sort_values(by="case_id")
 #data=data.drop(columns=["is_tumor","project_id"])
-#data.to_csv("./data/preprocessed_annotation_global.csv")
+data.to_csv("./data/preprocessed_annotation_global.csv")
 
 
 data = pd.read_csv(path_meth, sep='\t').transpose()
 data = data.reset_index()
 headers = data.iloc[0]
 data = pd.DataFrame(data.values[1:], columns=headers)
+
+index=data["Composite Element REF"]
 data_onlyvalues=data.drop(columns=["Composite Element REF"])
-data=data.drop(columns=data_onlyvalues.std().sort_values().head(10000).index)
+data=data[data_onlyvalues.std().sort_values(ascending=False).head(10000).index]
+data["Composite Element REF"]=index
+
 data= data[~data["Composite Element REF"].isin(cases_removed["case_id"]) ]
+data=data.sort_values(by="Composite Element REF")
 data.to_csv("./data/preprocessed_Matrix_meth.csv")
 
 data= pd.read_csv("./data/Matrix_miRNA_deseq_correct.csv", sep=',').transpose()
@@ -35,9 +41,14 @@ data = data.reset_index()
 headers = data.iloc[0]
 headers["index"]="Composite Element REF"
 data = pd.DataFrame(data.values[1:], columns=headers)
+
+index=data["Composite Element REF"]
 data_onlyvalues=data.drop(columns=["Composite Element REF"])
-data=data.drop(columns=data_onlyvalues.std().sort_values().head(10000).index)
+data=data[data_onlyvalues.std().sort_values(ascending=False).head(10000).index]
+data["Composite Element REF"]=index
+
 data= data[~data["Composite Element REF"].isin(cases_removed["case_id"]) ]
+data=data.sort_values(by="Composite Element REF")
 data.to_csv("./data/preprocessed_Matrix_miRNA_deseq_correct.csv")
 
 
@@ -48,9 +59,14 @@ data = data.reset_index()
 headers = data.iloc[0]
 headers["index"]="Composite Element REF"
 data = pd.DataFrame(data.values[1:], columns=headers)
+
+index=data["Composite Element REF"]
 data_onlyvalues=data.drop(columns=["Composite Element REF"])
-data=data.drop(columns=data_onlyvalues.std().sort_values().head(10000).index)
+data=data[data_onlyvalues.std().sort_values(ascending=False).head(10000).index]
+data["Composite Element REF"]=index
+
 data= data[~data["Composite Element REF"].isin(cases_removed["case_id"]) ]
+data=data.sort_values(by="Composite Element REF")
 data.to_csv("./data/preprocessed_Matrix_mRNA_deseq_normalized_prot_coding_correct.csv")
 
 
