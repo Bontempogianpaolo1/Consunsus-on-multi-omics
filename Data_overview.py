87b55d17af164cb8a4bf4c46a8716e6f5ef2bd64
mrfreak94 <bitrix9494@gmail.com>
2020-05-27 02:20:53 +0200

    data-overview

---
+++ Data_overview.py
@@ -0,0 +1,53 @@
+import pandas as pd
+path_annotation="./data/annotation_global.csv"
+path_meth="./data/Matrix_meth.csv"
+"""
+data = pd.read_csv(path_annotation, sep='\t')
+print("annotation")
+print("shape :" + str(data.shape))
+print("with tumor: "+ str(sum(data["is_tumor"]==1)))
+print("# tumor types: "+ str(len(data["project_id"].unique())))
+
+for type in data["project_id"].unique():
+    print("# of "+ type+ ": "+str(sum(data["project_id"]==type)))
+a=1
+"""
+"""
+data = pd.read_csv(path_meth, sep='\t').transpose()
+data = data.reset_index()
+headers = data.iloc[0]
+data = pd.DataFrame(data.values[1:], columns=headers)
+data_onlyvalues=data.drop(columns=["Composite Element REF"])
+print("Meth")
+print("shape :" + str(data.shape))
+print("mean per feature" +str(data_onlyvalues.mean().sort_values(ascending=True)))
+print("max per feature" +str(data_onlyvalues.max().sort_values(ascending=True)))
+print("min per feature" +str(data_onlyvalues.min().sort_values()))
+"""
+
+"""
+data= pd.read_csv("./data/Matrix_miRNA_deseq_correct.csv", sep=',').transpose()
+data = data.reset_index()
+headers = data.iloc[0]
+headers["index"]="Composite Element REF"
+data = pd.DataFrame(data.values[1:], columns=headers)
+data_onlyvalues=data.drop(columns=["Composite Element REF"])
+print("Matrix_miRNA_deseq_correct")
+print("shape :" + str(data.shape))
+print("mean per feature" +str(data_onlyvalues.mean().sort_values(ascending=True)))
+print("max per feature" +str(data_onlyvalues.max().sort_values(ascending=True)))
+print("min per feature" +str(data_onlyvalues.min().sort_values()))
+
+
+"""
+data= pd.read_csv("./data/Matrix_mRNA_deseq_normalized_prot_coding_correct.csv", sep=',').transpose()
+data = data.reset_index()
+headers = data.iloc[0]
+headers["index"]="Composite Element REF"
+data = pd.DataFrame(data.values[1:], columns=headers)
+data_onlyvalues=data.drop(columns=["Composite Element REF"])
+print("Matrix_miRNA_deseq_correct")
+print("shape :" + str(data.shape))
+print("mean per feature" +str(data_onlyvalues.mean().sort_values(ascending=True)))
+print("max per feature" +str(data_onlyvalues.max().sort_values(ascending=True)))
+print("min per feature" +str(data_onlyvalues.min().sort_values()))
