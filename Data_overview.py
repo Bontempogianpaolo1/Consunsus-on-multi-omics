f6d2132cdcf5a3ee9938e5e51449a23304376bd4
mrfreak94 <bitrix9494@gmail.com>
2020-05-27 12:45:51 +0200

    data-preparation

--- Data_overview.py
+++ Data_overview.py
@@ -1,18 +1,21 @@
 import pandas as pd
 path_annotation="./data/annotation_global.csv"
 path_meth="./data/Matrix_meth.csv"
-"""
+def union(row):
+    return str(row["is_tumor"])+"-"+row["project_id"]
 data = pd.read_csv(path_annotation, sep='\t')
 print("annotation")
 print("shape :" + str(data.shape))
 print("with tumor: "+ str(sum(data["is_tumor"]==1)))
 print("# tumor types: "+ str(len(data["project_id"].unique())))
-
+data["newlabel"]=data.apply(lambda row: union(row),axis=1)
 for type in data["project_id"].unique():
     print("# of "+ type+ ": "+str(sum(data["project_id"]==type)))
-a=1
-"""
-"""
+print("--------------------------------------------------------")
+for type in data["newlabel"].unique():
+    print("# of "+ type+ ": "+str(sum(data["newlabel"]==type)))
+
+
 data = pd.read_csv(path_meth, sep='\t').transpose()
 data = data.reset_index()
 headers = data.iloc[0]
@@ -23,9 +26,7 @@ print("shape :" + str(data.shape))
 print("mean per feature" +str(data_onlyvalues.mean().sort_values(ascending=True)))
 print("max per feature" +str(data_onlyvalues.max().sort_values(ascending=True)))
 print("min per feature" +str(data_onlyvalues.min().sort_values()))
-"""
 
-"""
 data= pd.read_csv("./data/Matrix_miRNA_deseq_correct.csv", sep=',').transpose()
 data = data.reset_index()
 headers = data.iloc[0]
@@ -39,14 +40,14 @@ print("max per feature" +str(data_onlyvalues.max().sort_values(ascending=True)))
 print("min per feature" +str(data_onlyvalues.min().sort_values()))
 
 
-"""
+
 data= pd.read_csv("./data/Matrix_mRNA_deseq_normalized_prot_coding_correct.csv", sep=',').transpose()
 data = data.reset_index()
 headers = data.iloc[0]
 headers["index"]="Composite Element REF"
 data = pd.DataFrame(data.values[1:], columns=headers)
 data_onlyvalues=data.drop(columns=["Composite Element REF"])
-print("Matrix_miRNA_deseq_correct")
+print("Matrix_mRNA_deseq_normalized_prot_coding_correct")
 print("shape :" + str(data.shape))
 print("mean per feature" +str(data_onlyvalues.mean().sort_values(ascending=True)))
 print("max per feature" +str(data_onlyvalues.max().sort_values(ascending=True)))
